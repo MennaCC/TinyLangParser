56e5466ad57758a077d5360abe4f7fdeadb0b871
Shimaa Lotfy <shimaalotfy627@gmail.com>
2016-12-12 12:04:11 +0200

    added_GrammarRules_base_class

--- Scanner/Parser/Logic/GrammarRules.cs
+++ Scanner/Parser/Logic/GrammarRules.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Scanner;
 
 namespace Parser.Logic
 {
-    class GrammarRules
+    public class GrammarRules : IGrammarRule
     {
+        public void execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MatchInput(Token t)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
     }
 }
